3f7d8de59d698f14cdb71d7791919e9a12509e4e
Dennis van Ee <dennis@vanee.us>
2017-10-26 11:32:54 -0700

    sync commit

--- ArtificialEye_2/src/Rendering/Subdivision.cpp
+++ ArtificialEye_2/src/Rendering/Subdivision.cpp
@@ -3,22 +3,42 @@
 #include <unordered_map>
 #include <set>
 
+const GLuint GLUINT_MAX = std::numeric_limits<GLuint>::max();
+
+class Edge
+{
+public:
+    GLuint p0() const { return m_p0; }
+    GLuint p1() const { return m_p1; }
+
+    void set(GLuint p0, GLuint p1) { sort(); }
+
+    bool operator==(const Edge& e) const { return m_p0 == e.m_p0 && m_p1 == e.m_p1; }
+       
+    Edge() : m_p0(GLUINT_MAX), m_p1(GLUINT_MAX) {}
+    Edge(GLuint p0, GLuint p1) : m_p0(p0), m_p1(p1) { sort(); }
+
+private:
+    void sort()
+    {
+        GLuint p0 = glm::min(m_p0, m_p1);
+        GLuint p1 = glm::max(m_p0, m_p1);
+        m_p0 = p0, m_p1 = p1;
+    }
+
+    GLuint m_p0;
+    GLuint m_p1;
+};
+
 struct edgePairHash 
 {
-    std::size_t operator() (const std::pair<GLuint, GLuint>& p) const 
+    std::size_t operator() (const Edge& p) const
     {
-        std::size_t p0 = p.first;
-        std::size_t p1 = p.second;
+        std::size_t p0 = static_cast<std::size_t>(p.p0());
+        std::size_t p1 = static_cast<std::size_t>(p.p1());
         return p0 | p1 << 32;
     }
 };
-
-std::pair<GLuint, GLuint> sortEdgePair(const std::pair<GLuint, GLuint>& p)
-{
-    GLuint min = glm::min(p.first, p.second);
-    GLuint max = glm::max(p.first, p.second);
-    return std::make_pair(min, max);
-}
 
 // This uses the catmull-clark subdivision algorithm to subdivide a mesh.
 void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& oFaces,
@@ -27,31 +47,31 @@ void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& o
     nVertices = oVertices;
 
     // table of edges and faces
-    std::unordered_map<std::pair<GLuint, GLuint>, std::vector<int>, edgePairHash> edgePointsTable;
+    std::unordered_map<Edge, std::vector<GLuint>, edgePairHash> edgePointsTable;
 
     struct FacePEdgeP
     {
-        int m_facePoint;
+        GLuint m_facePoint;
         
         struct EdgePP
         {
-            int m_edgePoint; // the edge point
-            std::pair<GLuint, GLuint> m_points; // the edge it is associated with
+            GLuint m_edgePoint; // the edge point
+            Edge m_edge; // the edge it is associated with
 
-            EdgePP() : m_edgePoint(-1), m_points(std::numeric_limits<GLuint>::max() , std::numeric_limits<GLuint>::max() ){}
+            EdgePP() : m_edgePoint(GLUINT_MAX) {}
         } m_edgePoints[3]; // always associated with 3 because there are three edges per face (it's a triangle)
 
-        FacePEdgeP() : m_facePoint(-1) {}
+        FacePEdgeP() : m_facePoint(GLUINT_MAX) {}
     };
     std::vector<FacePEdgeP> facePointPool;
     facePointPool.resize(oFaces.size()); // the number of faces that we will use
 
     // these points index into the face edge pool
-    std::vector<std::vector<int>> vertexFaces;
+    std::vector<std::vector<GLuint>> vertexFaces;
     vertexFaces.resize(nVertices.size());
    
     // assign all of the facePoints:
-    for (int faceID = 0; faceID < oFaces.size(); faceID++)
+    for (GLuint faceID = 0; faceID < oFaces.size(); faceID++)
     {
         const MeshFace& face = oFaces[faceID];
 
@@ -63,30 +83,31 @@ void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& o
 
         FacePEdgeP facePEdgeP;
         facePEdgeP.m_facePoint = nVertices.size() - 1;
-        facePEdgeP.m_edgePoints[0].m_points = sortEdgePair(std::make_pair(face.m_indices[0], face.m_indices[1]));
-        facePEdgeP.m_edgePoints[1].m_points = sortEdgePair(std::make_pair(face.m_indices[1], face.m_indices[2]));
-        facePEdgeP.m_edgePoints[2].m_points = sortEdgePair(std::make_pair(face.m_indices[2], face.m_indices[0]));
+        facePEdgeP.m_edgePoints[0].m_edge = Edge(face.m_indices[0], face.m_indices[1]);
+        facePEdgeP.m_edgePoints[1].m_edge = Edge(face.m_indices[1], face.m_indices[2]);
+        facePEdgeP.m_edgePoints[2].m_edge = Edge(face.m_indices[2], face.m_indices[0]);
             
+        // associate each vertex with another face
         facePointPool[faceID] = facePEdgeP;
-        vertexFaces[face.m_indices[0]].push_back(faceID); // denote what face belongs to this vertex
+        vertexFaces[face.m_indices[0]].push_back(faceID);
         vertexFaces[face.m_indices[1]].push_back(faceID);
         vertexFaces[face.m_indices[2]].push_back(faceID);
 
-        // assign edges to each of the points:
-        std::pair<GLuint, GLuint> edges[3];
-        for (int i = 0; i < 3; i++)
+        Edge edges[3] = 
         {
-            edges[i] = facePEdgeP.m_edgePoints[i].m_points;
-        }
+            facePEdgeP.m_edgePoints[0].m_edge,
+            facePEdgeP.m_edgePoints[1].m_edge,
+            facePEdgeP.m_edgePoints[2].m_edge
+        };
 
-        // associate them with edges
+        // set the faces for each edge (so, each edge gets 2 faces)
         for (int i = 0; i < 3; i++)
         {
             // check if the edge is already associated:
             if (edgePointsTable.find(edges[i]) == edgePointsTable.end())
             {
                 // add the facepoint to the list (by creating a new one first)
-                edgePointsTable.insert(std::make_pair(edges[i], std::vector<int>({faceID})));
+                edgePointsTable.insert(std::make_pair(edges[i], std::vector<GLuint>({faceID})));
             }
             // if so, add the new face (which is the face itself, not to be mistaken with 
             // the point
@@ -98,32 +119,29 @@ void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& o
     }
 
     // assign all the edge points
+    int count = 0;
     for (const auto& edges : edgePointsTable)
     {
-        FacePEdgeP faces[2] = {facePointPool[edges.second[0]], facePointPool[edges.second[1]]};
-        const std::pair<GLuint, GLuint>& endPoints = edges.first;
-
-        glm::vec3 edgePoint = (nVertices[faces[0].m_facePoint].m_position + nVertices[faces[1].m_facePoint].m_position
-            + nVertices[endPoints.first].m_position + nVertices[endPoints.second].m_position) * 0.25f;
+        FacePEdgeP* faces[2] = {&facePointPool[edges.second[0]], &facePointPool[edges.second[1]]};
+        Edge currEdge = edges.first;
+        glm::vec3 midEdge = (nVertices[currEdge.p0()].m_position + nVertices[currEdge.p1()].m_position) * 0.5f;
+        glm::vec3 midFace = (nVertices[faces[0]->m_facePoint].m_position + nVertices[faces[1]->m_facePoint].m_position) * 0.5f;
+        glm::vec3 edgePoint = (midEdge + midFace) * 0.5f;
         nVertices.push_back(edgePoint);
 
-        int minP_edge = glm::min(endPoints.first, endPoints.second);
-        int maxP_edge = glm::max(endPoints.first, endPoints.second);
-        
-        // loop through and update the vertices
+        // loop through and update the edgepoint as appropriate
         for (int j = 0; j < 2; j++)
         {
             for (int i = 0; i < 3; i++)
             {
-                int minP_face = glm::min(faces[j].m_edgePoints[i].m_points[0],
-                    faces[j].m_edgePoints[i].m_points[1]);
-                int maxP_face = glm::max(faces[j].m_edgePoints[i].m_points[0],
-                    faces[j].m_edgePoints[i].m_points[1]);
+                FacePEdgeP& currFace = *faces[j];
 
                 // this is the correct edge:
-                if (minP_edge == minP_face && maxP_edge == maxP_face)
+                if (currEdge == currFace.m_edgePoints[i].m_edge)
                 {
-                    faces[j].m_edgePoints[i].m_edgePoint = nVertices.size() - 1;
+                    // now calculate the edge point:
+                    assert(currFace.m_edgePoints[i].m_edgePoint == GLUINT_MAX);
+                    currFace.m_edgePoints[i].m_edgePoint = nVertices.size() - 1;
                     break;
                 }
             }
@@ -131,20 +149,21 @@ void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& o
     }
 
     // calculate the new points and do everything else, I guess
-    for (int oldPointID = 0; oldPointID < vertexFaces.size(); oldPointID++)
+    for (GLuint oldPointID = 0; oldPointID < vertexFaces.size(); oldPointID++)
     {
-        const std::vector<int>& faceIDs = vertexFaces[oldPointID];
+        const std::vector<GLuint>& faceIDs = vertexFaces[oldPointID];
         glm::vec3 oldPoint = nVertices[oldPointID].m_position;
 
         glm::vec3 aveFacePoint, aveEdgePoint;
-        std::set<int> edgePoints;
-        for (int faceID : faceIDs)
+        std::set<GLuint> edgePoints;
+        for (GLuint faceID : faceIDs)
         {
             aveFacePoint += nVertices[facePointPool[faceID].m_facePoint].m_position;
             edgePoints.insert(facePointPool[faceID].m_edgePoints[0].m_edgePoint);
             edgePoints.insert(facePointPool[faceID].m_edgePoints[1].m_edgePoint);
+            edgePoints.insert(facePointPool[faceID].m_edgePoints[2].m_edgePoint);
         }
-        for (int edgePointID : edgePoints)
+        for (GLuint edgePointID : edgePoints)
         {
             aveEdgePoint += nVertices[edgePointID].m_position;
         }
@@ -152,9 +171,9 @@ void ee::catmullClarkSubdiv(const VertBuffer& oVertices, const MeshFaceBuffer& o
         aveEdgePoint = aveEdgePoint * (1.f / edgePoints.size());
 
         float n = faceIDs.size();
-        float m1 = (n - 3) / n;
-        float m2 = 1 / n;
-        float m3 = 2 / n;
+        float m1 = (n - 3.f) / n;
+        float m2 = 1.f / n;
+        float m3 = 2.f / n;
 
         glm::vec3 newCoord = (m1 * oldPoint)
             + (m2 * aveFacePoint)
